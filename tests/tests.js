d1f2c5df36047e90f900ca5553ff07abda8358ea
Martin de Keijzer <mdkeijzer@ibuildings.nl>
2014-08-01 15:15:36 +0200

    Make test function named to make more sense of output

--- tests/tests.js
+++ tests/tests.js
@@ -1,4 +1,4 @@
-QUnit.test( "hello test", function( assert ) {
+QUnit.test( "hello test", function testInternal( assert ) {
     assert.ok( 1 == "1", "One is one" );
     assert.ok( 2 == "1", "2 is one" );
 });
